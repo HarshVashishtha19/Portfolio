b765ef366642ee99a8e024910fc5ed3bc9fc3349
HarshVashishtha19 <vashishthaharsh19@gmail.com>
2026-02-04 15:01:28 +0530

    Update

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -8,9 +8,9 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/b
 
 
 const Resume = () => {
-  const [wid, setwid] = useState(window.innerWidth);
+  const[wid, setwid]=useState(window.innerWidth);
 
-  const handleResize = () => {
+  const handleResize=()=>{
     setwid(window.innerWidth);
   }
 
@@ -20,17 +20,12 @@ const Resume = () => {
   return (
     <div className='ResumePage'>
       <Document file={pdf} className="resumeview">
-        <Page pageNumber={1} scale={wid < 700 ? (wid > 475 ? 0.7 : 0.5) : 1} />
+          <Page pageNumber= {1} scale={wid<700 ? ( wid>475? 0.7: 0.5): 1}/>
       </Document>
 
-      <a
-        href={pdf}
-        target="_blank"
-        rel="noopener noreferrer"
-        download="Harsh_Resume"
-      >
+      <a href={pdf} target='_blank' download="Harsh's Resume">
         <button className='downloadCV' type='button'>
-          <h3><BsDownload />&nbsp; Download CV</h3>
+          <h3><BsDownload/>&nbsp; Download CV</h3>
         </button>
       </a>
 
